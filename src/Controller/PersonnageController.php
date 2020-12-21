15210f78e402d3a6b949cefc356e2c090238d46c
Nassim <nassimkechida@gmail.com>
2020-12-21 19:03:39 +0100

    création application de personnages

---
+++ src/Controller/PersonnageController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Personnage;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonnageController extends AbstractController
+{
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function index(): Response
+    {
+        return $this->render('personnage/index.html.twig');
+    }
+
+    /**
+     * @Route("/persos", name="personnages")
+     */
+    public function persos(): Response
+    {
+        Personnage::creerPersonnages();
+        return $this->render('personnage/persos.html.twig',[
+            "players" => Personnage::$personnages 
+         ]);
+    }
+
+    /**
+     * @Route("/persos/{nom}", name="afficher_personnage")
+     */
+    public function afficherPerso($nom)
+    {
+        Personnage::creerPersonnages();
+        $perso = Personnage:: getPersonnageParNom($nom);
+        return $this->render('personnage/perso.html.twig',[
+            "perso" => $perso
+         ]);
+    }
+}
